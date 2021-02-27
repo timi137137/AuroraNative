e5616cefd76c2de260bb82576e20e8053cae8051
悠静萝莉 <i@mikuhl.cn>
2021-02-27 20:26:46 +0800

    Save Some File

---
+++ AuroraNavite/WebSocket/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuroraNavite.WebSocket
+{
+    /// <summary>
+    /// WebSocket 客户端 封装类
+    /// <para>正向 WebSocket</para>
+    /// </summary>
+    public class Client
+    {
+
+    }
+}
---
+++ AuroraNavite/WebSocket/Server.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuroraNavite.WebSocket
+{
+    /// <summary>
+    /// WebSocket 服务器 封装类
+    /// <para>反向WebSocket</para>
+    /// </summary>
+    public class Server
+    {
+    }
+}
