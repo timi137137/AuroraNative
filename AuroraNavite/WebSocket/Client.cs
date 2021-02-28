40fa7adef0da3536789347db68071352516f461f
悠静萝莉 <i@mikuhl.cn>
2021-02-28 17:27:15 +0800

    Update Some Files - Add WS Feedback

--- AuroraNavite/WebSocket/Client.cs
+++ AuroraNavite/WebSocket/Client.cs
@@ -1,9 +1,11 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace AuroraNavite.WebSocket
 {
@@ -24,25 +26,65 @@ namespace AuroraNavite.WebSocket
             set { Host = value; }
         }
 
+        private ClientWebSocket WebSocketClient;
+        private JObject Json;
+        private Task WaitFeedback;
+
         #endregion
 
-        public void aClient()
-        {
-            JObject Json = new JObject();
-            JObject SecJson = new JObject();
+        #region --构造函数--
 
-            Json.Add("action", "send_private_msg");
-            Json.Add("echo", "a");
-            SecJson.Add("user_id", "3220419645");
-            SecJson.Add("message", "test");
-            SecJson.Add("auto_escape", false);
-            Json.Add("params", SecJson);
-
-            CancellationToken token = new CancellationToken();
-            ClientWebSocket client = new ClientWebSocket();
-            client.ConnectAsync(new Uri("ws://127.0.0.1:6700/"), token);
-            client.SendAsync(new ArraySegment<byte>(System.Text.Encoding.UTF8.GetBytes(Json.ToString())), WebSocketMessageType.Text, true, token);
-            client.CloseAsync(WebSocketCloseStatus.NormalClosure, "-1", token);
+        /// <summary>
+        /// Client 类构建函数
+        /// </summary>
+        public Client() {
+            
         }
+
+        #endregion
+
+        #region --公开函数--
+
+        /// <summary>
+        /// 创建并连接到WebSocket服务器
+        /// </summary>
+        /// <returns>连接成功返回true，反而异之</returns>
+        public bool Create()
+        {
+            WebSocketClient = new ClientWebSocket();
+            Task Connect = WebSocketClient.ConnectAsync(new Uri("ws://" + Host + "/"), CancellationToken.None);
+            Connect.Wait();
+            if (WebSocketClient.State == WebSocketState.Open)
+            {
+                WaitFeedback = Task.Run(Feedback);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 立刻中断并释放连接<para>注意！断开后需要重新Create</para>
+        /// </summary>
+        public void Dispose() {
+            WebSocketClient.Dispose();
+            WebSocketClient.Abort();
+        }
+
+        #endregion
+
+        #region --私有函数--
+
+        private async void Feedback() {
+            while (true) {
+                ArraySegment<byte> BytesReceived = new ArraySegment<byte>(new byte[10240]);
+                WebSocketReceiveResult Result = await WebSocketClient.ReceiveAsync(BytesReceived, CancellationToken.None);
+                Json = JObject.Parse(Encoding.UTF8.GetString(BytesReceived.Array, 0, Result.Count));
+            }
+        }
+
+        #endregion
     }
 }
